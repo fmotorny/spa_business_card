46dcd8f22896f4c28092a083143cb944d832644b
Fedor <Fedor@DESKTOP-24RJST5>
2024-04-26 09:26:34 +0300

    Получаю список итемов

---
+++ client/src/app/api/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  public getProductsTest(): Observable<any> {
+    return this.http.get(environment.apiUrl + 'products')
+  }
+}
