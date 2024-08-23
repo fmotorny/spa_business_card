2d4cf70ab226adc357c13ee6723a05f9996bcbe4
Fedor <Fedor@DESKTOP-24RJST5>
2024-08-23 16:59:01 +0300

    Добавил призму орм для сбора лидов v0.1

--- client/src/server/api/tgMsg.ts
+++ client/src/server/api/tgMsg.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from '@prisma/client';
 import * as https from 'https';
 
 export const sendMsg = (req: any, res: any) => {
@@ -15,6 +16,29 @@ export const sendMsg = (req: any, res: any) => {
   https.get(
     `https://api.telegram.org/bot${process.env["TG_BOT_TOKEN"]}/sendMessage?chat_id=${process.env["CHAT_ID"]}&parse_mode=html&text=${msg}`,
     (response) => {
+
+
+     const prisma = new PrismaClient();
+
+      async function main() {
+        const user = await prisma.user.create({
+          data: {
+            name: 'Alice',
+            email: 'alice@prisma.io',
+          },
+        })
+        console.log(user)
+      }
+
+      main()
+        .then(async () => {
+          await prisma.$disconnect()
+        })
+        .catch(async (e) => {
+          console.error(e)
+          await prisma.$disconnect()
+          process.exit(1)
+        })
 
       console.log("statusCode:", response && response.statusCode);
    //   console.log("body:", body);
